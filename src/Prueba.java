a4774ac22f2cfd1e8f9f39f4d6c20f17d64f4967
MarioValiente2 <mario.valiente2@educa.madrid.org>
2023-12-22 15:59:57 +0100

    Subir Cambios

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,5 +2,8 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Adios");
+        System.out.println("Cambio1");
+        System.out.println("Cambio2");
+        System.out.println("Cambio3");
     }
 }
